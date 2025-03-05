9b5be9da56c09d35ca4b97e481a58715a07e145e
EMMAN PUNSALAN <eggpunsalan@gmail.com>
2025-03-05 23:47:22 +0800

    Orgs To Pick is now Connected to Local MongoDB

---
+++ models/orgs.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const orgSchema = new mongoose.Schema({
+    orgName: { type: String, required: true, unique: true },
+    orgPic: { type: String, default: "/images/icon.png" },
+    orgRating: { type: Number, default: 0 },
+    orgReviews: [{ type: Number }],
+    orgCollege: { type: String },
+    orgPassword: { type: String, required: true } 
+}, { timestamps: true });
+
+module.exports = mongoose.model('Organization', orgSchema);
