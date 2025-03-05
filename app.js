6429318897fd277358973ba1c6652df181e2cf43
Ezra Jeonadab G. Del Rosario <ezrajeonadabg@gmail.com>
2025-03-05 12:34:14 +0800

    implemented db for orgs

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const exphbs = require("express-handlebars");
 const path = require("path");
+mongoose.connect("mongodb://localhost:27017/orgs");
 
+const orgs = mongoose.connection.collection("organizations");
 const app = express();
 
 // handlebars
@@ -37,13 +39,22 @@ const orgPageRoutes = require('./routes/orgpage');
 app.use('/', homepageRoutes);
 app.use('/signup', signupRoutes);
 app.use('/login', loginRoutes);
-
 app.use('/searchorg', searchOrgRoutes);
 app.use('/searchreview', searchRevRoutes);
-
 app.use('/reviewpage', revPageRoutes);
 app.use('/reviewedit', revEditRoutes);
 app.use('/editorg', editOrgRoutes);
 app.use('/orgpage', orgPageRoutes);
 
+// Search org
+app.get("/search/org=:org", async (req, res) => {
+        try {
+            const org = await orgs.find({orgName: req.params.org}).toArray();
+            res.send({result: org});
+        }
+        catch (err) {
+            res.status(500).send("Error in Searching for " + req.params.org);
+        }
+
+    });
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
--- routes/searchorg.js
+++ routes/searchorg.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const mongoose = require("mongoose");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('searchorg', { title: 'Search Organizations', layout: 'main' });
+mongoose.connect("mongodb://localhost:27017/orgs");
+
+const orgs = mongoose.connection.collection("organizations");
+
+router.get('/', async (req, res) => {
+    try {
+        res.render('searchorg', { 
+            title: 'Search Organizations', 
+            layout: 'main',
+            orgList: await orgs.find().toArray()
+        });
+    }
+    catch (err) {
+        res.status(500).send("Error fetching organizations");
+    }
 });
 
 module.exports = router;
