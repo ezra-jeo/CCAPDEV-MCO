652800e3f0e156f5cc86b455f4239adccc630a1c
Ezra Jeonadab G. Del Rosario <ezrajeonadabg@gmail.com>
2025-03-06 00:06:35 +0800

    Readded orgs.js old version

---
+++ models/orgs.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+let orgsSchema = new mongoose.Schema({
+    orgName: {type: String, required: true},
+    orgPic: {type: String},
+    reviewPlural: Boolean,
+    orgRating: {type: Number, required: true, min: 1, max: 5 },
+    orgReviews: {type: Number, required: true, min: 0},
+    reviewDesc: {type: String},
+    orgCollege: {type: String, required: true}
+});
+
+let Orgs = mongoose.model("Organization", orgsSchema);
+module.exports = Orgs;
+
+/*
+const mongoose = require('mongoose');
+
+const orgSchema = new mongoose.Schema({
+    orgName: { type: String, required: true, unique: true },
+    orgPic: { type: String, default: "/images/icon.png" },
+    orgRating: { type: Number, default: 0 },
+    orgReviews: [{ type: Number }],
+    orgCollege: { type: String },
+    orgPassword: { type: String, required: true } 
+}, { timestamps: true });
+
+module.exports = mongoose.model('Organization', orgSchema);
+
+*/
