13c42e4825f6ace1db878fe3fda141dbdef3ee9c
Ezra Jeonadab G. Del Rosario <ezrajeonadabg@gmail.com>
2025-03-07 23:13:40 +0800

    feat: search and filter for org and reviews

--- app.js
+++ app.js
@@ -315,6 +315,142 @@ app.use('/', orgPageRoutes);
 
 app.use('/userpage', userPageRoutes);
 
+// Search and Filter
+app.get("/orgs/searchfilter/org:org?/qry1:qry1?/qry2:qry2?", async (req, res) => {
+    try {
+        let filterStars = [];
+        if (req.params.qry1) {
+            for (let filter of req.params.qry1.split(",")) {
+                if (filter >= "1" && filter <= "5") 
+                    filterStars.push(Number(filter));
+            }
+        }    
+
+        let filterCollege = [];
+        if (req.params.qry2) {
+            for (let filter of req.params.qry2.split(",")) {
+                filterCollege.push(filter);
+            }
+        }  
+
+        let query = {};
+        if (req.params.org) {
+            query["orgName"] = {$regex: new RegExp(req.params.org, "i")};
+        }   
+        if (filterStars.length > 0) {
+            query["orgRating"] = {$in: filterStars};
+        }
+        if (filterCollege.length > 0) {
+            query["orgCollege"] = {$in: filterCollege};
+        }
+        if (Object.keys(query).length == 0) {
+             query = {};
+        }
+        
+        const result = await Organization.find(query).lean();
+        res.send({orgList: result});
+        console.log("Org Sent Result " + result + " Query: " + query);
+
+    }
+    catch (err) {
+        res.status(500).send("Error in Searching and Filter");
+    }
+});
+
+app.get("/reviews/searchfilter/search:search?/qry1:qry1?/qry2:qry2?", async (req, res) => {
+    try {
+        let filterStars = [];
+        if (req.params.qry1) {
+            for (let filter of req.params.qry1.split(",")) {
+                if (filter >= "1" && filter <= "5") 
+                    filterStars.push(Number(filter));
+            }
+        }    
+
+        let filterCollege = [];
+        if (req.params.qry2) {
+            for (let filter of req.params.qry2.split(",")) {
+                filterCollege.push(filter);
+            }
+        }    
+
+        let query = {};
+        if (req.params.search) {
+            query["$or"] = [{"reviewText": {$regex: new RegExp(req.params.search, "i")}}, {"org.orgName": {$regex: new RegExp(req.params.search, "i")}}];
+        }   
+        if (filterStars.length > 0) {
+            query["reviewRating"] = {$in: filterStars};
+        }
+        if (filterCollege.length > 0) {
+            query["org.orgCollege"] = {$in: filterCollege};
+        }
+        if (Object.keys(query).length == 0) {
+             query = {};
+        }
+
+        const result = await Review.find(query).lean();
+        res.send({reviewList: result});
+        console.log("Review Sent Result " + result + " Query: " + query);
+    }
+    catch (err) {
+        res.status(500).send("Error in Searching and Filter");
+    }
+});
+
+
+// Sort
+
+app.get("/orgs/sort/org:org?/qry1:qry1?/qry2:qry2?/method:method/order:order", async (req, res) => {
+    try {
+
+        let filterStars = [];
+        if (req.params.qry1) {
+            for (let filter of req.params.qry1.split(",")) {
+                if (filter >= "1" && filter <= "5") 
+                    filterStars.push(Number(filter));
+            }
+        }    
+
+        let filterCollege = [];
+        if (req.params.qry2) {
+            for (let filter of req.params.qry2.split(",")) {
+                filterCollege.push(filter);
+            }
+        }    
+
+        let query = {};
+        if (req.params.org) {
+            query["orgName"] = {$regex: new RegExp(req.params.org, "i")};
+        }   
+        if (filterStars.length > 0) {
+            query["orgRating"] = {$in: filterStars};
+        }
+        if (filterCollege.length > 0) {
+            query["orgCollege"] = {$in: filterCollege};
+        }
+        if (Object.keys(query).length == 0) {
+             query = {};
+        }
+        
+        let order = {};
+        
+        if (req.params.method && req.params.method == "name") {
+            order = {"orgName": Number(req.params.order)};
+        }
+        else if (req.params.method && req.params.method == "rating") {
+            order = {"orgRating": Number(req.params.order)};
+        }
+
+        const result = await Organization.find(query).sort(order).lean();
+        res.send({orgList: result});
+        console.log("Org Sent Result " + result + " Query: " + query);
+
+    }
+    catch (err) {
+        res.status(500).send("Error in Sorting");
+    }
+});
+
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
 
 // closing db connection
