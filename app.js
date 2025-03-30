ed560a1eb485e4f3957943afade81b82e26f942b
Ong, Kyle Edward M. <168694603+Kemo1006@users.noreply.github.com>
2025-03-30 17:15:57 +0800

    fixed replying to review

--- app.js
+++ app.js
@@ -322,15 +322,22 @@ app.post("/reply-to-review", async (req, res) => {
             return res.status(400).json({ success: false, message: "Review ID or reply message is missing." });
         }
 
+        if (!req.session.user) {
+           return res.status(403).json({ success: false, message: "You must be logged in to reply"});
+        }
+
+        const responseUser = req.session.user.orgName || req.session.user.userName;
+
         // Update the review with the organization's response
         await Review.findByIdAndUpdate(reviewId, {
-            responseMessage: replyText
+            responseMessage: replyText,
+            responseUser: responseUser
         });
 
         res.json({ success: true, message: "Reply added successfully!" });
     } catch (err) {
         console.error("Error replying to review:", err);
-        res.status(500).json({ success: false, message: "Internal server error" });
+        res.status(500).json({ success: false, message: "Server error" });
     }
 });
 
